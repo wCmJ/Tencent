66fb3089451c84e95c2214bed93c4c7803532b4d
hahaha <younext@126.com>
2020-02-10 21:21:36 +0800

    Create tree.cpp

---
+++ sword/tree.cpp
@@ -0,0 +1,201 @@
+#include<iostream>
+#include<stack>
+
+namespace traversal{
+    /*
+        first: recursive
+        second: loop with stack
+        third: morris traversal    
+    */
+    struct treeNode{
+        int val;
+        treeNode *left;
+        treeNode *right;
+        treeNode(int v):val(v),left(NULL),right(NULL){}
+    };
+
+    void preorder(treeNode *root){
+        if(NULL != root){
+            std::cout<<root->val<<" ";
+            preorder(root->left);
+            preorder(root->right);
+        }
+    }
+    void inorder(treeNode *root){
+        if(NULL != root){
+            inorder(root->left);
+            std::cout<<root->val<<" ";
+            inorder(root->right);
+        }
+    }
+    void postorder(treeNode *root){
+        if(NULL != root){
+            postorder(root->left);
+            postorder(root->right);
+            std::cout<<root->val<<" ";
+        }
+    }
+
+    void preorderLoop(treeNode *root, std::stack<treeNode*> &s){
+        while(root){
+            std::cout<<root->val<<" ";//preorder output
+            if(root->right)s.push(root->right);
+            root = root->left;
+        }
+    }
+
+    void preorderLoop(treeNode *root){
+        std::stack<treeNode*> s;
+        while(true){
+            preorderLoop(root, s);
+            if(s.empty())
+                break;
+            root = s.top();
+            s.pop();    
+        }
+    }
+
+    void inorderLoop(treeNode *root, std::stack<treeNode*> &s){
+        while(root){
+            s.push(root);
+            root = root->left;
+        }
+    }
+    void inorderLoop(treeNode *root){
+        std::stack<treeNode*> s;
+        while(true){
+            inorderLoop(root, s);
+            if(s.empty())
+                break;
+            root = s.top();
+            s.pop();
+            std::cout<<root->val<<" ";//inorder output
+            root = root->right;    
+        }
+    }
+    void postorderLoop(treeNode *root, std::stack<treeNode*> &s, std::stack<int> &data){
+        while(root){
+            data.push(root->val);
+            if(root->left)s.push(root->left);
+            root = root->right;
+        }
+    }
+    void postorderLoop(treeNode *root){
+        std::stack<treeNode*> s;
+        std::stack<int> data;
+        while(true){
+            postorderLoop(root, s, data);
+            if(s.empty())
+                break;
+            root = s.top();
+            s.pop();        
+        }
+        while(!data.empty()){
+            std::cout<<data.top() <<" ";//postorder output
+            data.pop();
+        }
+    }
+
+    void morrisTraversal(treeNode *root){
+        while(root){
+            //get precursor
+            treeNode *mostRight = root->left;
+            if(mostRight != NULL){
+                while(mostRight->right != NULL && mostRight->right != root){
+                    mostRight = mostRight->right;
+                }
+                if(mostRight->right == NULL){//first time
+                    mostRight->right = root;
+                    root = root->left;
+                    continue;
+                }
+                else{//second time
+                    mostRight->right = NULL;
+                }
+            }
+            root = root->right;    
+        }
+    }
+
+    void morrisTraversalPreorder(treeNode *root){
+        while(root){
+            treeNode *mostRight = root->left;
+            if(mostRight != NULL){
+                while(mostRight->next != NULL && mostRight->next != root){
+                    mostRight = mostRight->right;
+                }
+                if(mostRight->next == NULL){
+                    std::cout<<root->val<<" ";
+                    mostRight->next = root;
+                    root = root->left;
+                    continue;
+                }
+                else{
+                    mostRight->next = NULL;
+                }
+            }
+            else{
+                std::cout<<root->val<<" ";
+            }
+            root = root->right;
+        }
+    }
+
+    void morrisTraversalInorder(treeNode *root){
+        while(root){
+            treeNode *mostRight = root->left;
+            if(mostRight != NULL){
+                while(mostRight->right != NULL && mostRight->right != root){
+                    mostRight = mostRight->right;
+                }
+                if(mostRight->right == NULL){//first time
+                    mostRight->right = root;
+                    root = root->left;
+                    continue;
+                }
+                else{
+                    mostRight->right = NULL;
+                }
+            }
+            else{
+                //
+            }
+            std::cout<<root->val<<" ";
+            root = root->right;
+        }
+    }
+    void printReverse(treeNode *root){
+        if(root != NULL){
+            if(root->right != NULL){
+                printReverse(root->right);
+            }
+            std::cout<<root->val<<" ";
+        }
+    }
+    void morrisTraversalPostorder(treeNode *root){
+        treeNode *tmp = root;
+        while(root){
+            treeNode *mostRight = root->left;
+            if(mostRight != NULL){
+                while(mostRight->right != NULL && mostRight->right != root){
+                    mostRight = mostRight->right;
+                }
+                if(mostRight->right == NULL){
+                    mostRight->right = root;
+                    root = root->left;
+                    continue;
+                }
+                else{
+                    mostRight->right = NULL;
+                    //output 
+                    printReverse(root->left);
+                }
+            }
+            else{
+
+            }
+            root = root->right;
+        }
+        printReverse(tmp);
+    }
+}
