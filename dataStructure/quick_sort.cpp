278f9f6954a81761d558770c9710ba82f716fa1a
hahaha <younext@126.com>
2020-04-21 20:36:33 +0800

    Create quick_sort.cpp

---
+++ dataStructure/quick_sort.cpp
@@ -0,0 +1,45 @@
+void swap(int arr[], int x, int y){
+	int tmp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = tmp;
+}
+
+
+void quickSort(int arr[], int start, int end){
+	if(arr == NULL || start >= end)return;
+	int idx = end;
+	int less = start - 1;
+	int i = start;
+	while(i < idx){
+		if(arr[i] < arr[idx]){
+			swap(arr, ++less, i);
+		}
+		++i;
+	}
+	swap(arr, ++less, idx);
+	quickSort(arr, start, less - 1);
+	quickSort(arr, less + 1, end);
+}
+
+
+void quickSortAdv(int arr[], int start, int end){
+	if(arr == NULL || start >= end)return;
+	int less = start - 1;
+	int more = end;
+	int idx = end;
+	int i = start;
+	while(i < more){
+		if(arr[i] < arr[idx]){
+			swap(arr, ++less, i++);
+		}
+		else if(arr[i] > arr[idx]){
+			swap(arr, --more, i);
+		}
+		else{
+			++i;
+		}
+	}
+	swap(arr, more++, idx);
+	quickSortAdv(arr, start, less);
+	quickSortAdv(arr, more, end);
+}
